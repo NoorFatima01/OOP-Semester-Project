cfaf4151529e79499fcc2234d0704a412745042d
Amama-Fatima <princessamama321@gmail.com>
2023-11-25 13:01:37 +0500

    connected to database

--- src/main/java/com/electiondatabase/App.java
+++ src/main/java/com/electiondatabase/App.java
@@ -1,13 +1,24 @@
 package com.electiondatabase;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document; // Import the Document class
+
+public class App {
+    public static void main(String[] args) {
+        String connectionString = "mongodb://localhost:27017";
+
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            MongoDatabase database = mongoClient.getDatabase("election");
+            System.out.println("Connected to the database successfully");
+
+            MongoCollection<Document> votersCollection = database.getCollection("voters");
+            MongoCollection<Document> candidatesCollection = database.getCollection("candidates");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
