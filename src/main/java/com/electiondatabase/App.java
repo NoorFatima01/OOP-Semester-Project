baa3a99a4b86a34bcfff10bb548a0354f7152ce2
Noor Fatima <nfatima101204@gmail.com>
2023-11-25 19:55:06 +0500

    connected app to mongodb atlas cluster instead of a local connection

--- src/main/java/com/electiondatabase/App.java
+++ src/main/java/com/electiondatabase/App.java
@@ -5,11 +5,26 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import org.bson.Document; // Import the Document class
+import org.bson.Document;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class App {
     public static void main(String[] args) {
-        String connectionString = "mongodb://localhost:27017";
+        Properties prop = new Properties();
+        String connectionString;
+
+        try (InputStream input = new FileInputStream("config.properties")) {
+            // Load properties file
+            prop.load(input);
+            // Get the connection string from properties file
+            connectionString = prop.getProperty("mongo.connection.string");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return; // Stop the execution if the connection string can't be loaded
+        }
 
         try (MongoClient mongoClient = MongoClients.create(connectionString)) {
             MongoDatabase database = mongoClient.getDatabase("election");
@@ -19,7 +34,6 @@ public class App {
             MongoCollection<Document> candidatesCollection = database.getCollection("candidates");
 
             Application.launch(MainMenu.class, args);
-
 
         } catch (Exception e) {
             e.printStackTrace();
