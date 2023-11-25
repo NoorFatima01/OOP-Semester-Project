20de32a89bd6f9c140a654c761ff087c8e0c5e86
Noor Fatima <nfatima101204@gmail.com>
2023-11-25 13:00:51 +0500

    added main menu form

--- src/main/java/com/electiondatabase/App.java
+++ src/main/java/com/electiondatabase/App.java
@@ -1,13 +1,11 @@
 package com.electiondatabase;
+import javafx.application.Application;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Application.launch(MainMenu.class, args);
     }
 }
