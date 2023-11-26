8444a6a7e110b4b2756edb22c731f2a88496003a
Noor Fatima <nfatima101204@gmail.com>
2023-11-26 09:46:49 +0500

    Removed id from constructor

--- src/main/java/com/electiondatabase/Candidate.java
+++ src/main/java/com/electiondatabase/Candidate.java
@@ -4,7 +4,7 @@ public class Candidate extends User {
     private int votes;
     private String party;
 
-    public Candidate(int id, String firstName, String lastName, String region, String party) {
+    public Candidate(String firstName, String lastName, String region, String party) {
         super(firstName, lastName, region);
         this.party = party;
         this.votes = 0;
