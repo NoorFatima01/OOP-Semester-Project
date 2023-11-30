a978439847271322be6d96d5f28c15a36d907adf
Noor Fatima <nfatima101204@gmail.com>
2023-11-30 18:16:51 +0500

    deleted unnecessary csv files and code

--- src/main/java/com/electiondatabase/App.java
+++ src/main/java/com/electiondatabase/App.java
@@ -4,17 +4,15 @@ import javafx.application.Application;
 
 import com.electiondatabase.forms.MainMenu;
 
-public class App{
+public class App {
     public static void main(String[] args) {
-        String workingDir = System.getProperty("user.dir");
-        System.out.println("Current working directory: " + workingDir);
         try {
 
             Application.launch(MainMenu.class, args);
 
         } catch (Exception e) {
             e.printStackTrace();
-            
+
         }
-}
+    }
 }
